f240ed4147f471ce565f12a896eb14fd9688a69c
Julian22077 <camilolozano0122@gmail.com>
2026-04-19 04:42:39 +0000

    commit 3

---
+++ src/Original/index.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { Chart as ChartJS } from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+interface Datos{
+  id:number
+  name:
+}
+
+function Original(){
+
+}
